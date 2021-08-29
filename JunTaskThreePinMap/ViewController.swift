27934b338d1526b6e284048106f32770850853e3
Igor Guryan <sacujo@mail.ru>
2021-08-29 19:59:28 +0300

    init

--- JunTaskThreePinMap/ViewController.swift
+++ JunTaskThreePinMap/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let mapView : MKMapView = {
+        let mapVIew = MKMapView()
+        mapVIew.translatesAutoresizingMaskIntoConstraints = false
+        return mapVIew
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
 
 }
 
+extension ViewController {
+    
+    func setConstraints() {
+        
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+        
+        ])
+    }
+}
